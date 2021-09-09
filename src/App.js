79ed73f007b36cc31d4608b5603ebfe1f1c51845
anis jridi <jridi.anis.2014@gmail.com>
2021-09-09 17:11:12 +0100

    jsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import './App.css';
-import "./styles.css"
-import anis1 from "./anis1.jpg"
+import "./App.css";
+import "./styles.css";
+ import anis1 from "./images/anis1.jpg"
 
 function App() {
   return (
     <div className="App">
       <h1 classNames="titleRed">JRIDI ANIS</h1>
 
-      <img className="img" src="/anis1.jpg" alt="anis1" />
-
-      <img className="img" src="/anis 2" alt="anis 2" />
-
-</div>
+      <img className="img1" src={anis1} alt="anis1" />
 
 
 
-<iframe width="700" height="394" src="https://www.youtube.com/embed/SGMZhdNdxM4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <img className="img1" src="/anis2.jpg" alt="" />
 
 
+
+      <iframe width="700" height="394" src="https://www.youtube.com/embed/SGMZhdNdxM4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
     </div>
   );
